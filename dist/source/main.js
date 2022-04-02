1fad2775ff8eaa184f867633baf449a685b0d2fe
taeyangcode <coreymosterogithub@gmail.com>
2022-04-01 18:02:25 -0700

    Multiple choice, text area, participation question solvers completed

--- dist/source/main.js
+++ dist/source/main.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 debugger;
 const isUndefined = (input) => typeof input === "undefined";
 class Zybook {
@@ -27,21 +36,101 @@ class Zybook {
     static getCheckButtons() {
         return document.getElementsByClassName("zb-button primary raised check-button");
     }
+    static getRadioButtons() {
+        return document.querySelectorAll("input[type=radio]");
+    }
+    static getParticipateStartButtons() {
+        return document.getElementsByClassName("zb-button primary raised start-button start-graphic");
+    }
+    static get2XSpeedButtons() {
+        return document.querySelectorAll('input[aria-label="2x speed"]');
+    }
+    static participateQuestionsExist() {
+        return this.get2XSpeedButtons().length > 0;
+    }
+    static getParticipatePlayButtons() {
+        return document.getElementsByClassName("play-button bounce");
+    }
+    static getParticipateFinishButtons() {
+        return document.getElementsByClassName("play-button rotate-180");
+    }
 }
-function solve() {
-    const showAnswerButtons = Zybook.getShowAnswerButtons();
-    const forfeitAnswers = Zybook.getForfeitAnswers();
-    const textAreas = Zybook.getDefaultTextAreas();
-    const checkButtons = Zybook.getCheckButtons();
-    setTimeout(() => Zybook.clickButtons(showAnswerButtons), 2000);
-    setTimeout(() => Zybook.clickButtons(showAnswerButtons), 3000);
-    setTimeout(() => {
-        [...textAreas].forEach((textArea, index) => {
-            textArea.value = [...forfeitAnswers][index].textContent;
-            textArea.focus();
-            textArea.blur();
-            setTimeout(() => [...checkButtons][index].click(), 2000);
+class Solver {
+    static solveMultipleChoice() {
+        setTimeout(() => {
+            const radioButtons = Zybook.getRadioButtons();
+            radioButtons.forEach((radioButton, index) => {
+                setTimeout(() => radioButton.click(), index * 300);
+            });
+        }, 3000);
+    }
+    static solveTextAreas() {
+        const showAnswerButtons = Zybook.getShowAnswerButtons();
+        const forfeitAnswers = Zybook.getForfeitAnswers();
+        const textAreas = Zybook.getDefaultTextAreas();
+        const checkButtons = Zybook.getCheckButtons();
+        setTimeout(() => Zybook.clickButtons(showAnswerButtons), 1500);
+        setTimeout(() => Zybook.clickButtons(showAnswerButtons), 2000);
+        setTimeout(() => {
+            [...textAreas].forEach((textArea, index) => {
+                textArea.value = [...forfeitAnswers][index].textContent;
+                textArea.focus();
+                textArea.blur();
+                [...checkButtons][index].click();
+            });
+        }, 2500);
+    }
+    static enable2XSpeed(speedButtons) {
+        speedButtons.forEach((button) => {
+            button.click();
         });
-    }, 4000);
+    }
+    static completeParticipateActivity(activityAmount) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let finishButtons = Zybook.getParticipateFinishButtons();
+            // Run loop while the amount of finish buttons on the screen is less than the amount
+            // of participation activities for this page.
+            while (finishButtons.length < activityAmount) {
+                const playButtons = Zybook.getParticipatePlayButtons();
+                [...playButtons].forEach((playButton) => {
+                    playButton.click();
+                });
+                // Update finishButtons with the (new) amount of finish buttons on the page
+                finishButtons = Zybook.getParticipateFinishButtons();
+                // To prevent locking, loop pauses for 1 second before continuing onto next iteration
+                yield this.pause(1000);
+            }
+        });
+    }
+    static solveParticipateQuestions() {
+        setTimeout(() => {
+            // If there are no participate questions, exit function
+            if (!Zybook.participateQuestionsExist()) {
+                return;
+            }
+            const startButtons = Zybook.getParticipateStartButtons();
+            const speedButtons = Zybook.get2XSpeedButtons();
+            const activityAmount = speedButtons.length;
+            this.enable2XSpeed(speedButtons);
+            // Click start button for each participation activity
+            [...startButtons].forEach((startButton, index) => {
+                // To avoid issue where click event is sometimes
+                // skipped when looping over HTML elements too fast,
+                // each click is separated by 300 ms.
+                setTimeout(() => {
+                    startButton.click();
+                }, 300 * index);
+            });
+            // Initiate the completion of each activity after
+            // each activity has been started.
+            // (hence the + 100ms)
+            setTimeout(() => {
+                this.completeParticipateActivity(activityAmount);
+            }, 300 * startButtons.length + 100);
+        }, 3000);
+    }
 }
-solve();
+Solver.pause = (time) => new Promise((resolve) => setTimeout(resolve, time));
+Solver.solveMultipleChoice();
+Solver.solveTextAreas();
+Solver.solveParticipateQuestions();
--- source/main.ts
+++ source/main.ts
@@ -35,25 +35,125 @@ class Zybook {
   public static getCheckButtons(): HTMLCollectionOf<HTMLButtonElement> {
     return document.getElementsByClassName("zb-button primary raised check-button") as HTMLCollectionOf<HTMLButtonElement>;
   }
+
+  public static getRadioButtons(): NodeListOf<HTMLInputElement> {
+    return document.querySelectorAll("input[type=radio]");
+  }
+
+  public static getParticipateStartButtons(): HTMLCollectionOf<HTMLButtonElement> {
+    return document.getElementsByClassName("zb-button primary raised start-button start-graphic") as HTMLCollectionOf<HTMLButtonElement>;
+  }
+
+  public static get2XSpeedButtons(): NodeListOf<HTMLInputElement> {
+    return document.querySelectorAll('input[aria-label="2x speed"]');
+  }
+
+  public static participateQuestionsExist(): boolean {
+    return this.get2XSpeedButtons().length > 0;
+  }
+
+  public static getParticipatePlayButtons(): HTMLCollectionOf<HTMLDivElement> {
+    return document.getElementsByClassName("play-button bounce") as HTMLCollectionOf<HTMLDivElement>;
+  }
+
+  public static getParticipateFinishButtons(): HTMLCollectionOf<HTMLDivElement> {
+    return document.getElementsByClassName("play-button rotate-180") as HTMLCollectionOf<HTMLDivElement>;
+  }
+
+  // public static getOutputSolverStartButton(): HTMLCollectionOf<HTMLButtonElement> {
+  //   return document.getElementsByClassName("zyante-progression-start-button button") as HTMLCollectionOf<HTMLButtonElement>;
+  // }
 }
 
-function solve() {
-  const showAnswerButtons: HTMLCollectionOf<HTMLButtonElement> = Zybook.getShowAnswerButtons();
-  const forfeitAnswers: HTMLCollectionOf<HTMLSpanElement> = Zybook.getForfeitAnswers();
-  const textAreas: HTMLCollectionOf<HTMLTextAreaElement> = Zybook.getDefaultTextAreas();
-  const checkButtons: HTMLCollectionOf<HTMLButtonElement> = Zybook.getCheckButtons();
+class Solver {
+  public static solveMultipleChoice(): void {
+    setTimeout(() => {
+      const radioButtons: NodeListOf<HTMLInputElement> = Zybook.getRadioButtons();
 
-  setTimeout(() => Zybook.clickButtons(showAnswerButtons), 2000);
-  setTimeout(() => Zybook.clickButtons(showAnswerButtons), 3000);
+      radioButtons.forEach((radioButton: HTMLInputElement, index: number) => {
+        setTimeout(() => radioButton.click(), index * 300);
+      });
+    }, 3000);
+  }
 
-  setTimeout(() => {
-    [...textAreas].forEach((textArea: HTMLTextAreaElement, index: number) => {
-      textArea.value = [...forfeitAnswers][index].textContent!;
-      textArea.focus();
-      textArea.blur();
-      setTimeout(() => [...checkButtons][index].click(), 2000);
+  public static solveTextAreas(): void {
+    const showAnswerButtons: HTMLCollectionOf<HTMLButtonElement> = Zybook.getShowAnswerButtons();
+    const forfeitAnswers: HTMLCollectionOf<HTMLSpanElement> = Zybook.getForfeitAnswers();
+    const textAreas: HTMLCollectionOf<HTMLTextAreaElement> = Zybook.getDefaultTextAreas();
+    const checkButtons: HTMLCollectionOf<HTMLButtonElement> = Zybook.getCheckButtons();
+
+    setTimeout(() => Zybook.clickButtons(showAnswerButtons), 1500);
+    setTimeout(() => Zybook.clickButtons(showAnswerButtons), 2000);
+
+    setTimeout(() => {
+      [...textAreas].forEach((textArea: HTMLTextAreaElement, index: number) => {
+        textArea.value = [...forfeitAnswers][index].textContent!;
+        textArea.focus();
+        textArea.blur();
+        [...checkButtons][index].click();
+      });
+    }, 2500);
+  }
+
+  private static enable2XSpeed(speedButtons: NodeListOf<HTMLInputElement>): void {
+    speedButtons.forEach((button: HTMLInputElement) => {
+      button.click();
     });
-  }, 4000);
+  }
+
+  private static pause = (time: number) => new Promise((resolve: TimerHandler) => setTimeout(resolve, time));
+
+  private static async completeParticipateActivity(activityAmount: number): Promise<void> {
+    let finishButtons: HTMLCollectionOf<HTMLDivElement> = Zybook.getParticipateFinishButtons();
+
+    // Run loop while the amount of finish buttons on the screen is less than the amount
+    // of participation activities for this page.
+    while (finishButtons.length < activityAmount) {
+      const playButtons: HTMLCollectionOf<HTMLDivElement> = Zybook.getParticipatePlayButtons();
+      [...playButtons].forEach((playButton: HTMLDivElement) => {
+        playButton.click();
+      });
+
+      // Update finishButtons with the (new) amount of finish buttons on the page
+      finishButtons = Zybook.getParticipateFinishButtons();
+
+      // To prevent locking, loop pauses for 1 second before continuing onto next iteration
+      await this.pause(1000);
+    }
+  }
+
+  public static solveParticipateQuestions(): void {
+    setTimeout(() => {
+      // If there are no participate questions, exit function
+      if (!Zybook.participateQuestionsExist()) {
+        return;
+      }
+
+      const startButtons: HTMLCollectionOf<HTMLButtonElement> = Zybook.getParticipateStartButtons();
+      const speedButtons: NodeListOf<HTMLInputElement> = Zybook.get2XSpeedButtons();
+      const activityAmount: number = speedButtons.length;
+      this.enable2XSpeed(speedButtons);
+
+      // Click start button for each participation activity
+      [...startButtons].forEach((startButton: HTMLButtonElement, index: number) => {
+        // To avoid issue where click event is sometimes
+        // skipped when looping over HTML elements too fast,
+        // each click is separated by 300 ms.
+        setTimeout(() => {
+          startButton.click();
+        }, 300 * index);
+      });
+
+      // Initiate the completion of each activity after
+      // each activity has been started.
+      // (hence the + 100ms)
+      setTimeout(() => {
+        this.completeParticipateActivity(activityAmount);
+      }, 300 * startButtons.length + 100);
+    }, 3000);
+  }
 }
 
-solve();
+Solver.solveMultipleChoice();
+Solver.solveTextAreas();
+Solver.solveParticipateQuestions();
